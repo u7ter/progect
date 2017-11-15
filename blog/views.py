3f41c4049cf180902b8d9537640a1102ff7bc72b
Maksim Mysak <maksimmysak@gmail.com>
2017-11-16 00:21:04 +0200

    add post model

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.order_by('created_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
